5cd7507f494f9c5785228568c212c68c76e93f5d
Richie <rahait.expert@gmail.com>
2024-02-07 22:59:16 +0100

    Splitting code structure into components via slots

---
+++ src/components/Grid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid grid-cols-1 lg:grid-cols-12 gap-12">
+    <div class="box mt-10 p-5 col-span-5">
+      <div class="grid grid-cols-2">
+        <div class="flex gap-2 flex-col">
+          <slot name="buttons"></slot>
+        </div>
+        <slot name="inputelements"></slot>
+      </div>
+    </div>
+
+    <div class="box mt-10 p-3 col-span-7">
+      <slot name="info"></slot>
+    </div>
+
+    <div class="box col-start-6 col-end-13">
+      <slot name="messagesequence"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.box {
+  border-radius: 7px;
+  box-shadow: 0 0 10px 7px #89d8f0;
+}
+</style>
--- src/pages/ISO2.vue
+++ src/pages/ISO2.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="container">
-    <div class="grid grid-cols-1 lg:grid-cols-12  gap-12">
-      <div class="box mt-10 p-5 col-span-5 ">
-        <div class="grid grid-cols-2">
-          <div class="flex gap-2 flex-col">
-            <PLugIn />
-            <Stop />
-            <PauseCharging />
-            <ResumeCharging />
-            <Renegotiation />
-            <Unplug />
-          </div>
-          <InputForm />
-        </div>
-      </div>
-      <div class="box mt-10 p-3 col-span-7">sas</div>
-      <div class="box col-start-6 col-end-13">sasas</div>
-    </div>
+    <Grid>
+      <template v-slot:buttons>
+        <PLugIn />
+        <Stop />
+        <PauseCharging />
+        <ResumeCharging />
+        <Renegotiation />
+        <Unplug />
+      </template>
+      <template v-slot:inputelements>
+        <InputForm />
+      </template>
+
+      <template v-slot:info>
+        <div>sas</div>
+      </template>
+
+      <template v-slot:messagesequence>
+        <div class="text-center text-white text-4xl my-4">AC-Messagesequence</div>
+      </template>
+    </Grid>
   </div>
 </template>
 
@@ -28,11 +32,7 @@ import ResumeCharging from "../components/Buttons/ResumeCharging.vue";
 import Renegotiation from "../components/Buttons/Renegotiation.vue";
 import Unplug from "../components/Buttons/Unplug.vue";
 import InputForm from "../components/InputForm.vue";
+import Grid from "../components/Grid.vue";
 </script>
 
-<style scoped>
-.box {
-  border-radius: 7px;
-  box-shadow: 0 0 10px 7px orange;
-}
-</style>
+<style scoped></style>
