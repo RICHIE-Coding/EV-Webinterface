8ba04406b39591bd5c9c7c6908f979a0145a9d30
Richie <rahait.expert@gmail.com>
2024-04-08 17:31:50 +0200

    feat: create RadioButton.vue component

--- src/components/RadioButton.vue
+++ src/components/RadioButton.vue
@@ -4,13 +4,21 @@
       <div>
         <input id="default-radio-1" type="radio" value="" name="default-radio"
           class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
-        <label for="default-radio-1" class="ms-2 text-md font-medium text-white">ISO 15118</label>
+        <label for="default-radio-1" class="ms-2 text-md font-medium text-white">{{ props.title1 }}</label>
       </div>
       <div>
         <input checked id="default-radio-2" type="radio" value="" name="default-radio"
           class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
-        <label for="default-radio-2" class="ms-2 text-md font-medium text-white">Basic Charging</label>
+        <label for="default-radio-2" class="ms-2 text-md font-medium text-white">{{ props.title2 }}</label>
       </div>
     </div>
   </div>
 </template>
+<script setup lang="ts">
+
+const props = defineProps({
+  title1: String,
+  title2: String,
+});
+
+</script>
--- src/pages/ISO2.vue
+++ src/pages/ISO2.vue
@@ -10,17 +10,20 @@
         <Button title="Unplug-Car" buttonColor="gray" />
       </template>
       <template v-slot:settings>
-        <TCPTLSSelection />
-        <ISO15118BasicChargingSelection />
+        <RadioButton title1="TCP" title2="TLS" />
+        <RadioButton title1="ISO 15118" title2="Basic Charging" />
         <SetPauseState />
         <ChargingWithoutContract />
         <InputForm />
       </template>
 
       <template v-slot:info>
-        <div class="text-white text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa omnis tempora quis
-          perspiciatis ducimus corporis officia? Dolorum iusto error porro necessitatibus, voluptatum pariatur quaerat,
-          sapiente impedit odio unde molestias est.</div>
+        <div class="text-white text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa omnis
+          tempora quis perspiciatis ducimus corporis officia? Dolorum iusto
+          error porro necessitatibus, voluptatum pariatur quaerat, sapiente
+          impedit odio unde molestias est.
+        </div>
       </template>
 
       <template v-slot:messagesequence>
@@ -31,14 +34,13 @@
 </template>
 
 <script setup lang="ts">
-import Button from "../components/Button.vue"
+import RadioButton from "../components/RadioButton.vue"
+import Button from "../components/Button.vue";
 import InputForm from "../components/InputForm.vue";
-import TCPTLSSelection from "../components/RadioButtons/TCPTLSSelection.vue"
-import ISO15118BasicChargingSelection from "../components/RadioButtons/ISO15118BasicChargingSelection.vue"
-import ChargingWithoutContract from "../components/Checkbox/ChargingWithoutContract.vue"
-import SetPauseState from "../components/Checkbox/SetPauseState.vue"
+import ChargingWithoutContract from "../components/Checkbox/ChargingWithoutContract.vue";
+import SetPauseState from "../components/Checkbox/SetPauseState.vue";
 import Grid from "../components/Grid.vue";
-import ISO2MessageSequence from "../components/MessageSequences/ISO2Messagesequence.vue"
+import ISO2MessageSequence from "../components/MessageSequences/ISO2Messagesequence.vue";
 </script>
 
 <style scoped></style>
